3d2f347e95e9ed284360526bf296d7668f4cd734
Ryota MIBU <r-mibu@cq.jp.nec.com>
2015-04-20 09:29:54 +0900

    Add requirement doc drop1
    Change-Id: Ibfd83f84ae47291ece09150af9f35d38ef3a1288
    JIRA: PROMISE-1
    Signed-off-by: Ryota MIBU <r-mibu@cq.jp.nec.com>

---
+++ requirements/resource_management/conf.py
@@ -0,0 +1,23 @@
+import datetime
+
+extensions = ['sphinxcontrib.plantuml']
+plantuml = ['java', '-jar', 'plantuml.jar']
+
+source_suffix = '.rst'
+master_doc = 'index'
+pygments_style = 'sphinx'
+html_use_index = False
+
+pdf_documents = [('index', u'Promise', u'Promise Project', u'OPNFV')]
+plantuml_latex_output_format = 'eps'
+pdf_fit_mode = "shrink"
+pdf_stylesheets = ['sphinx','kerning','a4']
+#latex_domain_indices = False
+#latex_use_modindex = False
+latex_elements = {'printindex': ''}
+latex_appendices = ['glossary', '07-schemas']
+
+project = u'Promise: Resource Management'
+copyright = u'%s, OPNFV' % datetime.date.today().year
+version = u'0.0.1'
+release = u'0.0.1'
