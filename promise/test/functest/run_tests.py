fe5b31f939c3b600fb46c30d427eb08a89f7ea21
Linda Wang <wangwulin@huawei.com>
2017-06-02 08:38:03 +0000

    Fix the wrong image path
    The images are put in the dir /home/opnfv/functest/images now.
    
    Change-Id: I15b41782cd7a7372bdb962da575edc5d1b54d509
    Signed-off-by: Linda Wang <wangwulin@huawei.com>

--- promise/test/functest/run_tests.py
+++ promise/test/functest/run_tests.py
@@ -46,7 +46,7 @@ PROMISE_FLAVOR_DISK = CONST.promise_flavor_disk
 GLANCE_IMAGE_FILENAME = CONST.openstack_image_file_name
 GLANCE_IMAGE_FORMAT = CONST.openstack_image_disk_format
 GLANCE_IMAGE_NAME = CONST.openstack_image_file_name
-GLANCE_IMAGE_PATH = os.path.join(CONST.dir_functest_data,
+GLANCE_IMAGE_PATH = os.path.join(CONST.dir_functest_images,
                                  GLANCE_IMAGE_FILENAME)
 
 PROMISE_NET_NAME = CONST.promise_network_name
